b8714101f54251dc0611143feaa7dceb0bf0402c
trisimix <trisimix@protonmail.com>
2019-03-13 18:43:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-wget https://javadl.oracle.com/webapps/download/AutoDL?BundleId=236878_42970487e3af4f5aa5bca3f542482c60
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3a%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" "https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz"
