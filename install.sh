26ac5ed8bd823594ed17a74b4adb440b859c53aa
trisimix <trisimix@protonmail.com>
2019-03-14 13:29:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #/bin/bash
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3a%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" "https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz"
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+export JAVA_HOME=SCRIPTPATH/../jre1.8.0_201/
+export PATH=JAVA_HOME/bin:$PATH
+screen -d -m LaunchServer.sh 
