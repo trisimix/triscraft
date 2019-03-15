7debaaefc116d4ca269cd5b1474f1090053aeaea
Ubuntu <ubuntu@ip-172-31-1-10.us-east-2.compute.internal>
2019-03-15 16:05:09 +0000

    backup

--- LaunchServer.sh
+++ LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -server -Xmx3G -XX:PermSize=256M -Dfml.queryResult=confirm -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+../jre8/bin/java -server -Xmx3G -XX:PermSize=256M -Dfml.queryResult=confirm -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
--- installaunch.sh
+++ installaunch.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPTPATH="`pwd`"
 export JAVA_HOME=SCRIPTPATH/../jre1.8.0_201/
 export PATH=JAVA_HOME/bin:$PATH
+chmod +x LaunchServer.sh
 screen -d -m LaunchServer.sh 
