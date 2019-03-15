8590f6832d73c18a8b6932ed8bbe0b1067752500
Ubuntu <ubuntu@ip-172-31-1-10.us-east-2.compute.internal>
2019-03-15 17:33:25 +0000

    launchscrept

--- LaunchServer.sh
+++ LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../jre8/bin/java -server -Xmx3G -XX:PermSize=256M -Dfml.queryResult=confirm -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+/usr/lib/jvm/jdk1.8.0_201/bin/java  -server -Xmx3G -XX:PermSize=256M -Dfml.queryResult=confirm -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
